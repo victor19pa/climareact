400f6aa68c9b74023a180fad05a52da9af7b3d04
victor19pa <vpinedaa@unah.hn>
2022-02-14 22:48:43 -0600

    creacion proyecto

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <Text>APP JS</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
